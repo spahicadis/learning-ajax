e1541b0e4f77fd806c966fb8bf20805cd56e8e4d
spahicadis <adis@spahica.com>
2024-09-06 19:46:46 +0200

    Old method XMLHTTPRequest, Promises, AJAX

---
+++ script.js
@@ -0,0 +1,72 @@
+
+/*
+let xhttpGet = new XMLHttpRequest();
+xhttpGet.onreadystatechange = function () {
+  if(this.readyState == 4 && this.status == 200 ) {
+    console.log(xhttpGet.responseText);
+  }
+}
+
+xhttpGet.open("GET", "https://66dac436f47a05d55be5e489.mockapi.io/Users", true);xhttpGet.send();
+//starija metoda
+
+let xhttpPost = new XMLHttpRequest();
+xhttpPost.onreadystatechange = function () {
+  if(this.readyState == 4 && this.status == 200 ) {
+    console.log(xhttpPost.responseText);
+  }
+}
+  */
+
+/*
+xhttpPost.open("POST", "https://66dac436f47a05d55be5e489.mockapi.io/Users", true);xhttpPost.send(
+  JSON.stringify({
+    "createdAt": Date.now,
+    "name": "Hope Cartwright",
+    "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/238.jpg",
+    "id": "22"
+  })
+);
+*/
+
+const inputForma = document.querySelector('.userID');
+const btnForme = document.querySelector('.fetchBtn');
+const display = document.querySelector('.displayUser');
+
+let inputValue;
+let matchedId
+
+inputForma.addEventListener('input', (e) => {
+  inputValue = e.target.value;
+  console.log(inputValue);
+})
+
+btnForme.addEventListener('click', (e) => {
+  loadUsers();
+  inputForma.value = ''
+ 
+})
+
+ function loadUsers() { fetch('https://66dac436f47a05d55be5e489.mockapi.io/Users').then(response => response.json().then(data => {
+  data.forEach((user) => {
+    if(user.id === inputValue) {
+      display.innerHTML += `<p><b>${user.name}</b></p>
+      <img src='${user.avatar}'/>`
+
+      
+    }
+  })
+})).catch(error => {
+  alert(error);
+})
+ }
+
+
+//console.log(r); //znaci vraca obecanje i kada to obecanje bude ispunjeno mi cemo moci vidjeti podatke 
+
+/*
+let response = request.then(response => response.json());
+console.log(response); //znaci ovo ovako nece raditi jer je ovo asinhrono
+*/
+
+
